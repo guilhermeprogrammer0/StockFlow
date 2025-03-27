740a3cc4dfa40e5fe91feb8dbd7a31fbdba9f232
Guilherme Souza <guilherme003souza@gmail.com>
2025-03-27 18:12:48 -0300

    Reajustes

--- BANCO/stockflow.sql
+++ BANCO/stockflow.sql
@@ -2,7 +2,7 @@
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           8.4.3 - MySQL Community Server - GPL
 -- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.8.0.6908
+-- HeidiSQL Versão:              12.10.0.7000
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -14,13 +14,37 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+-- Copiando estrutura do banco de dados para stockflow
+CREATE DATABASE IF NOT EXISTS `stockflow` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `stockflow`;
+
+-- Copiando estrutura para tabela stockflow.migrations
+CREATE TABLE IF NOT EXISTS `migrations` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Copiando dados para a tabela stockflow.migrations: ~1 rows (aproximadamente)
 INSERT IGNORE INTO `migrations` (`id`, `migration`, `batch`) VALUES
 	(1, '2025_03_25_175752_create_users_table', 1);
 
+-- Copiando estrutura para tabela stockflow.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `usuario` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `senha` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 -- Copiando dados para a tabela stockflow.users: ~1 rows (aproximadamente)
 INSERT IGNORE INTO `users` (`id`, `nome`, `usuario`, `senha`, `created_at`, `updated_at`) VALUES
-	(1, 'Guilherme Souza', 'gui@mail.com', '$2y$12$.4RSBhwZImppZW9tTo7XwO0RH9EDU/hEH8.fm3pBjOw.nDQTHLgzi', NULL, NULL);
+	(1, 'Guilherme Souza', 'gui@mail.com', '$2y$12$LDzIjVgxgw62VQZBih.uVeJmHLJDIBAdbfWS7/1NMltb7qOfZT486', NULL, NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
