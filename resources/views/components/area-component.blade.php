29ea7428ff74155305932f6cd1570b5edc0b7838
Guilherme Souza <guilherme003souza@gmail.com>
2025-05-19 12:20:25 -0300

    OK

--- resources/views/components/area-component.blade.php
+++ resources/views/components/area-component.blade.php
@@ -1,3 +1,3 @@
-<section class="area h-full overflow-y-auto flex justify-center items-start gap-2 flex-wrap ">
+<section class="area h-full overflow-y-auto flex justify-center items-start gap-2 flex-wrap">
     {{$slot}}
 </section>
--- resources/views/livewire/busca-produtos.blade.php
+++ resources/views/livewire/busca-produtos.blade.php
@@ -1,13 +1,13 @@
 <x-area-component>
     <div class="w-4/5 flex justify-center items-center flex-col p-2">
-            <label for="busca" class="color-text text-2xl text-center"> Buscar </label>
-            <input type="text" class="w-4/5 md:w-1/2 p-3 bg-white outline-none" wire:model.live="busca" placeholder="Código ou nome">
+        <label for="busca" class="text-2xl text-center"> Buscar </label>
+        <input type="text" class="w-4/5 md:w-1/2 p-3 bg-white outline-none" wire:model.live="busca" placeholder="Código ou nome">
         <p class="text-center color text-base p-1 mt-2 text-white rounded-sm w-1/2">Total de produtos encontrados: <strong>{{$quantidadeProdutos}}</strong></p>
     </div>
     @if(count($produtos) === 0)
     <h2 class="text-white bg-red-900 p-1 text-center mt-2 text-3xl">Não foram encontrados produtos</h2>
     @else
-    <div class="flex gap-1 w-full h-auto justify-evenly items-center flex-wrap">
+    <div class="flex gap-1 w-full h-auto justify-evenly items-start flex-wrap">
         @foreach($produtos as $produto)
         <div class="card w-auto">
             <div class="card-header relative">
